2c158cf28133dd976e069a391a8ee3d966bd8d1c
M_mirzaei <M_mirzaei.NL@proton.me>
2023-07-17 23:13:13 +0330

    step 4 Done

--- myscript.js
+++ myscript.js
@@ -5,4 +5,30 @@ function getComputerChoice(){
   const choice = options [Math.floor(Math.random() * options.length)];
   return choice;
 }
+
+function CheckWinner(playerSelection , computerSelection){
+  if (playerSelection == computerSelection){
+    return "Tie !";
+  }
+  else if ( (playerSelection == "Rock" && computerSelection == "Scissors")||
+    (playerSelection == "Scissors" && computerSelection == "Paper")||
+    (playerSelection == "Paper" && computerSelection == "Rock")
+  ){return "player"}
+  else{return "computer"}
+}
+function playRound(playerSelection , computerSelection) {
+  const result = CheckWinner(playerSelection , computerSelection);
+  if (result == "Tie !"){
+    return "It's a Tie !!!"
+  }
+  else if (result == "player") {
+    return `You Won! ${playerSelection} beats ${computerSelection}`
+  }
+  else {
+    return `You LOSE ! ${computerSelection} beats ${playerSelection}`
+  }
+} 
+
+
+
 getComputerChoice()
